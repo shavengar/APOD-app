8c3abb9fb9bd8f153242bdad2950bb103409aed9
Sydney Havengar <sydney.havengar01@gmail.com>
2022-03-18 14:16:49 -0500

    adds navigation based on user and private routes

--- src/components/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Navigate } from "react-router-dom";
 
-const ProtectedRoutes = () => {
-    return <div></div>;
+const ProtectedRoutes = ({ isPrivate, user, children }) => {
+    const redirectTo = isPrivate ? "/login" : "/home";
+    //determines access to components depending on if they are logged in or not
+    if ((isPrivate && user) || (!isPrivate && !user)) {
+        return <>{children}</>;
+    } else {
+        return <Navigate to={redirectTo} />;
+    }
 };
 
-export default ProtectedRoutes;
+const mapStateToProps = (state) => {
+    return {
+        user: state.user,
+    };
+};
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProtectedRoutes);
