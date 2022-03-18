27bc1fd735ab71d689fcfdcda3c6ae99be26d475
Sydney Havengar <sydney.havengar01@gmail.com>
2022-03-18 14:17:18 -0500

    adds navlinks for page navigation

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+import { clearUser } from "../redux/actions";
 
-const Menu = () => {
-    return <div></div>;
+const Menu = ({ user, clearUser }) => {
+    return (
+        <div>
+            {!user && (
+                <>
+                    <NavLink to="login">Login</NavLink>
+                    <NavLink to="signup">Sign Up</NavLink>
+                </>
+            )}
+            {user && (
+                <>
+                    <NavLink to="home">Home</NavLink>
+                    <NavLink to="saved">Saved</NavLink>
+                    <NavLink
+                        to="login"
+                        onClick={() => {
+                            clearUser();
+                        }}
+                    >
+                        Logout
+                    </NavLink>
+                </>
+            )}
+        </div>
+    );
 };
 
-export default Menu;
+const mapStateToProps = (state) => {
+    return {
+        user: state.user,
+    };
+};
+const mapDispatchToProps = {
+    clearUser,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Menu);
