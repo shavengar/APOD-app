71f7d6aa2a48b90781bbf5585495d84ff8deee77
Sydney Havengar <sydney.havengar01@gmail.com>
2022-03-18 11:59:58 -0500

    changes name

--- src/components/FavoritesPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FavoritesPage = () => {
-    return <div>Favorites Page</div>;
-};
-
-export default FavoritesPage;
---
+++ src/components/SavedPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SavedPage = () => {
+    return <div>Saved Page</div>;
+};
+
+export default SavedPage;
