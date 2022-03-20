d3143b45464d6c4120b006eb65dd38f8c000652f
Sydney Havengar <sydney.havengar01@gmail.com>
2022-03-20 12:40:47 -0500

    adds developer notes

--- src/hooks/useAPOD.jsx
+++ src/hooks/useAPOD.jsx
@@ -4,6 +4,13 @@ import React, { useState, useEffect } from "react";
 const baseURL =
     "https://api.nasa.gov/planetary/apod?api_key=RROYlTO9r3RaaFal3W7ZidAlk20Z8LaBiHaRDOs7&start_date=2021-09-19";
 
+/**
+ * asynchronously calls APOD api using axios
+ * --! note that there are some videos, will have to handle differently
+ * @throws error if unable to return api response
+ * @returns copyright, date (using for id since unique), explanation, media type, title, and url
+ */
+
 const useAPOD = () => {
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
@@ -17,9 +24,9 @@ const useAPOD = () => {
             try {
                 const res = await axios.get(baseURL);
                 const apods = res.map((apod) => ({
-                    copyright: apod.copyright,
+                    apod_copyright: apod.copyright,
                     apod_id: apod.date,
-                    info: apod.explanation,
+                    apod_info: apod.explanation,
                     media_type: apod.media_type,
                     apod_title: apod.title,
                     apod_url: apod.url,
