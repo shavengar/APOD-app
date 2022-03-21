aab94ce4bb20f0ec2b01620840618d6bce4bf13a
Sydney Havengar <sydney.havengar01@gmail.com>
2022-03-20 19:04:20 -0500

    adds sizing for each image and title/author bar

--- src/components/APODDisplay.jsx
+++ src/components/APODDisplay.jsx
@@ -1,7 +1,16 @@
+import { ImageListItem, ImageListItemBar } from "@mui/material";
 import React from "react";
 
-const APODDisplay = () => {
-    return <div></div>;
+const APODDisplay = ({ media, mediaType, copyright, title, date, info }) => {
+    return (
+        <ImageListItem>
+            <img src={`${media}?w=248&fit=crop&auto=format`} alt={title} />
+            <ImageListItemBar
+                title={title}
+                subtitle={`Credit & Copyright: ${copyright}`}
+            />
+        </ImageListItem>
+    );
 };
 
 export default APODDisplay;
