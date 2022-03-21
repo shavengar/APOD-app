e61e84d612c91d5a1f76a801d095ba1e0e2bd100
Sydney Havengar <sydney.havengar01@gmail.com>
2022-03-20 19:06:06 -0500

    Handles loading, errors, and clean display of data

--- src/components/APODPage.jsx
+++ src/components/APODPage.jsx
@@ -1,7 +1,37 @@
+import { Box, ImageList } from "@mui/material";
 import React from "react";
+import useAPOD from "../hooks/useAPOD";
+import APODDisplay from "./APODDisplay";
 
 const APODPage = () => {
-    return <div>APOD Page</div>;
+    const { data, error, loading } = useAPOD();
+    return (
+        <section>
+            <h1>NASA's Astronomy Photos of the Day:</h1>
+            <h4>Below are the photos since the start of 2022.</h4>
+            {loading && <div>Loading</div>}
+            {error && <div>Something went wrong.</div>}
+            {!error && !loading && (
+                <div className="displayFlex justifyCenter">
+                    <Box sx={{ width: 1000, height: 700, overflowY: "scroll" }}>
+                        <ImageList variant="masonry" cols={3} gap={8}>
+                            {data.map((apod) => (
+                                <APODDisplay
+                                    key={apod.apod_id}
+                                    media={apod.apod_url}
+                                    mediaType={apod.media_type}
+                                    copyright={apod.apod_copyright}
+                                    title={apod.apod_title}
+                                    date={apod.apod_date}
+                                    info={apod.apod_info}
+                                />
+                            ))}
+                        </ImageList>
+                    </Box>
+                </div>
+            )}
+        </section>
+    );
 };
 
 export default APODPage;
