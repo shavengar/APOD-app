8a56395e625cf365070276d65b8e77e0f90cb1cd
Sydney Havengar <sydney.havengar01@gmail.com>
2022-03-21 11:17:40 -0500

    adds buttons to save and unsave

--- src/components/APODDisplay.jsx
+++ src/components/APODDisplay.jsx
@@ -1,14 +1,35 @@
-import { ImageListItem, ImageListItemBar } from "@mui/material";
+import { Button, ImageListItem, ImageListItemBar } from "@mui/material";
 import React from "react";
 
-const APODDisplay = ({ media, mediaType, copyright, title, date, info }) => {
+const APODDisplay = ({
+    apod,
+    media,
+    mediaType,
+    copyright,
+    title,
+    date,
+    info,
+    isFavorite,
+    addFavorite,
+    removeFavorite,
+    apod_id,
+}) => {
     return (
         <ImageListItem>
             <img src={`${media}?w=248&fit=crop&auto=format`} alt={title} />
-            <ImageListItemBar
-                title={title}
-                subtitle={`Credit & Copyright: ${copyright}`}
-            />
+            {!isFavorite && (
+                <Button variant="outlined" onClick={() => addFavorite(apod)}>
+                    Save
+                </Button>
+            )}
+            {isFavorite && (
+                <Button
+                    variant="contained"
+                    onClick={() => removeFavorite(apod_id)}
+                >
+                    Remove Saved
+                </Button>
+            )}
         </ImageListItem>
     );
 };
