25512d972b97fd539c54f365231744b1a53ff1ac
Sydney Havengar <sydney.havengar01@gmail.com>
2022-03-22 13:55:58 -0500

    removes curly braces from setting user

--- src/redux/reducers/user.reducer.js
+++ src/redux/reducers/user.reducer.js
@@ -5,7 +5,7 @@ const initialState = null;
 const userReducer = (state = initialState, action) => {
     switch (action.type) {
         case SET_USER:
-            return { ...action.username };
+            return action.username;
         case CLEAR_USER:
             return null;
         default:
