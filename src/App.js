8328932bc5cae143db65a553eda724a61b874597
Sydney Havengar <sydney.havengar01@gmail.com>
2022-03-25 16:50:24 -0500

    implements useAPI hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 import {
     BrowserRouter as Router,
     Routes,
@@ -12,8 +13,35 @@ import LoginPage from "./components/LoginPage";
 import SignupPage from "./components/SignupPage";
 import APODPage from "./components/APODPage";
 import SavedPage from "./components/SavedPage";
+import useAPI from "./hooks/useAPI";
+import { setUser } from "./redux/actions/user.actions";
+import { setFavorites } from "./redux/actions/apod.actions";
 
-function App() {
+function App({ user, setUser, setFavorites }) {
+    const { getFavesByUser, verify } = useAPI();
+
+    useEffect(() => {
+        const getFavorites = async () => {
+            if (user) {
+                const res = await getFavesByUser();
+                if (res.data.success) {
+                    setFavorites(res.data.data);
+                }
+            }
+        };
+        getFavorites();
+    }, [user]);
+
+    useEffect(() => {
+        const checkUser = async () => {
+            const res = await verify();
+            if (res.data.success) {
+                setUser(res.data.data);
+            }
+        };
+        checkUser();
+    }, []);
+
     return (
         <Router>
             <Menu />
@@ -56,4 +84,12 @@ function App() {
     );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+    return {
+        user: state.user,
+    };
+};
+
+const mapDispatchToProps = { setUser, setFavorites };
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
