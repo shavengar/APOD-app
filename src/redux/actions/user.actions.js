ed8eac20eaab8dbb4731d1891e9ca19077b4e3f3
Sydney Havengar <sydney.havengar01@gmail.com>
2022-03-25 21:52:12 -0500

    edits typo

--- src/redux/actions/user.actions.js
+++ src/redux/actions/user.actions.js
@@ -1,5 +1,5 @@
 export const SET_USER = "Set User";
-export const CLEAR_USER = "Set User";
+export const CLEAR_USER = "Clear User";
 
 export const setUser = (username) => {
     return { type: SET_USER, username };
