ccb91301e93766a229110c79a593c5331ee8f47d
Sydney Havengar <sydney.havengar01@gmail.com>
2022-03-25 23:08:25 -0500

    fixes port typo

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const PORT = process.env.port || 8080;
+const PORT = process.env.PORT || 8080;
 const authRoutes = require("./server/routes/auth.routes");
 const favoritesRoutes = require("./server/routes/favorites.routes");
 const passport = require("./server/config/passport.config");
